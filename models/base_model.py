d9606e7c37db48b197658856a02be3cc71cab120
adams miracle <adamsmiracle0@gmail.com>
2024-01-16 16:00:49 +0000

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -21,31 +21,33 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-        
+
     def save(self):
         """
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute
+        updated_at with the current datetime
         """
         self.updated_at = datetime.utcnow()
-        
+
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
-        
+
         return inst_dict
-    
+
     def __str__(self):
         """
         Returns string representation of the instance
         """
         return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
-    
-    
+
+
 if __name__ == "__main__":
     model = BaseModel()
     model.name = "My first model"
